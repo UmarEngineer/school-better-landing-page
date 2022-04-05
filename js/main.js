4fda6a4aa2ad6f128981b65f4e44a6af29272978
obidcan <ohojimirzayev@gmail.com>
2022-04-05 11:32:16 +0500

    Correcting

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     const NavbarToggler = document.querySelector('.js-navbar__toggle');
     const NavbarItems = document.querySelector('.navbar-items');
     const NavbarButtons = document.querySelector('.navbar-buttons'); 
-
+    var hamburgerIcon = document.querySelector('.hamburger-icon');
     if (NavbarToggler) {
 
     NavbarToggler.addEventListener('click', function(){
